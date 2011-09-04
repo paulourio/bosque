6fefe2a0a25598bdb3a28d44354a5738379cac84
Paulo Roberto Urio <jimmyskull@gmail.com>
2011-09-04 15:35:29 -0400

    New option for output file name Signed-off-by: Paulo Roberto Urio <jimmyskull@gmail.com>

--- desenhar.sh
+++ desenhar.sh
@@ -1,19 +1,58 @@
+#!/bin/bash
 
+function usage
+{
+    echo "usage: desenhar [-o outputfile] [tree values]"
+}
+
+ext="svg"
 DIR=`pwd`
 DIR=$DIR/arvores
-if [ ! -d "$DIR" ] ; then
-	mkdir $DIR
-fi
-
-cd $DIR
-
 FILE=$DIR/bstree-$RANDOM
 FILEPDFCROP=$FILE-crop.pdf
-FINAL=$FILE.svg
+FINAL=$FILE.$ext
+CUSTOM=0
+
+trees=""
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -o )            shift
+                        FINAL=$1
+                        CUSTOM=1
+                        if [ "$FINAL" = "" ]; then
+                            echo "You must define an output file."
+                            exit 1
+                        fi
+                        ;;
+        -h | --help )   usage
+                        exit
+                        ;;
+        * )             trees="$trees $1"
+                        ;;
+    esac
+    shift
+done
+
+if [ $CUSTOM -eq 0 ]; then
+    if [ ! -d "$DIR" ] ; then
+        mkdir $DIR
+    fi
+
+    cd $DIR
+else
+    EXT=`echo "$FINAL" | awk -F . '{print $NF}' | tr 'A-Z' 'a-z'`
+    if [ "$EXT" != "$ext" ]; then
+        FINAL=$FINAL.svg
+    fi
+    DIR=`pwd`
+    FILE=$DIR/bstree-$RANDOM
+    FILEPDFCROP=$FILE-crop.pdf
+fi
 
 RM=`which rm`
 
-if [ $# -eq 0 ]; then
+if [ "$trees" = "" ]; then
 	echo "Sintaxe: desenhar [árvore em pré-ordem]"
 	echo "Exemplo: desenhar 5 3 1 4 6"
 	exit 1
@@ -55,32 +94,42 @@ if [ ! -f $DRAW ]; then
 	exit 1
 fi
 
-$DRAW $@ > $FILE.tex
+$DRAW $trees > $FILE.tex
 if [ $? -ne 0 ]; then
 	echo "Erro ao gerar árvore"
 	exit 1
 fi
+
 $PDFLATEX $FILE > /dev/null
 if [ $? -ne 0 ]; then
 	echo "Erro executar latex"
+    $RM $FILE.tex $FILE.aux $FILE.log
 	exit 1
 fi
+
 $RM $FILE.tex $FILE.aux $FILE.log
+
 $PDFCROP $FILE.pdf $FILEPDFCROP > /dev/null
 if [ $? -ne 0 ]; then
 	echo "Erro ao executar pdfcrop"
+    $RM $FILE.pdf
 	exit 1
 fi
+
 $RM $FILE.pdf
+
 $PDF2SVG $FILEPDFCROP $FINAL
 if [ $? -ne 0 ]; then
 	echo "Erro ao converter para SVG"
+    $RM $FILEPDFCROP
 	exit 1
 fi
+
 $RM $FILEPDFCROP
-$OPEN $FINAL
 
 echo "$FINAL"
+$OPEN $FINAL
 
-cd ..
-#rm $FINAL
+if [ $CUSTOM -eq 0 ]; then
+    cd ..
+fi
